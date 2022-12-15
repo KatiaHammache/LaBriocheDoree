6381583e31090578d2e0a6975ed0950bf2eb60c8
Mehdi Chebbah <i16.chebbah@gmail.com>
2022-12-15 04:00:45 +0100

    feat: page front

--- bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/resources/PaymentResource.java
+++ bank/src/main/java/fr/pantheonsorbonne/ufr27/miage/resources/PaymentResource.java
@@ -5,6 +5,9 @@ import fr.pantheonsorbonne.ufr27.miage.exception.ClientNotFoundException;
 import fr.pantheonsorbonne.ufr27.miage.exception.PasswordIncorrectException;
 import fr.pantheonsorbonne.ufr27.miage.exception.SoldUnsifficientException;
 import fr.pantheonsorbonne.ufr27.miage.service.PaymentService;
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -18,11 +21,40 @@ public class PaymentResource {
     @Inject
     PaymentService paymentService;
 
-    @Path("client/{clientId}/password/{password}")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response cardPayment(@PathParam("clientId") Integer clientId, @PathParam("password") Integer password) throws ClientNotFoundException, SoldUnsifficientException, PasswordIncorrectException {
-        return Response.ok(paymentService.cardPayment(clientId,password)).build();
+    @Location("paymentTemplate.html")
+    Template paymentTemplate;
+
+    @Location("doneTemplate.html")
+    Template doneTemplate;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get(){
+        return paymentTemplate.instance();
     }
+
+    @Path("authentification")
+    @POST
+    @Consumes(MediaType.TEXT_HTML)
+    public Response cardPayment(@FormParam("clientId") Integer clientId, @FormParam("password") Integer password)  {
+        try {
+
+            return Response.accepted(paymentService.cardPayment(clientId,password)).build();
+
+        } catch (ClientNotFoundException e) {
+            throw new WebApplicationException(404);
+        } catch (PasswordIncorrectException e) {
+            throw new WebApplicationException(404);
+        } catch (SoldUnsifficientException e) {
+            throw new WebApplicationException(404);
+        }
+    }
+//
+//    @Path("client/{clientId}/password/{password}")
+//    @POST
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response cardPayment(@PathParam("clientId") Integer clientId, @PathParam("password") Integer password) throws ClientNotFoundException, SoldUnsifficientException, PasswordIncorrectException {
+//        return Response.ok(paymentService.cardPayment(clientId,password)).build();
+//    }
 
 }
